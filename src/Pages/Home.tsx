8a244ad9f80efc02fff1149287024a4d0f71f396
Amit Prakash <prakashamit37@gmail.com>
2023-11-03 16:22:33 +0530

    Added footer and banners for home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,24 +1,35 @@
 import MoviePoster from "../Assets/M1.jpg"
+import HomeBanners from "../components/HomeBanners";
 import HomeCarousel from "../components/HomeCarousel";
 import HomeMovieCard from "../components/HomeMovieCard";
 import Navbar from "../components/Navbar";
+import IccBannerImage from '../Assets/p1.jpg';
+import HomeFooter from "../components/HomeFooter";
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-        <Navbar />
-        <HomeCarousel />
+            <Navbar />
+            <HomeCarousel />
+            <div className="min-h-[96rem] w-[80vw] mx-auto flex flex-col">
+            <HomeBanners image={IccBannerImage} />
+            <div className='text-2xl font-semibold text-black mt-4'>
+                Recommended Movies
+            </div>
+                <div className="mt-8 flex justify-center items-center gap-10">
+                    <HomeMovieCard movieImage={MoviePoster} />
+                    <HomeMovieCard movieImage={MoviePoster} />
+                    <HomeMovieCard movieImage={MoviePoster} />
+                    <HomeMovieCard movieImage={MoviePoster} />
+                    <HomeMovieCard movieImage={MoviePoster} />
 
-        <div className="mt-8 flex justify-center items-center gap-10">
-            <HomeMovieCard movieImage={MoviePoster}/>
-            <HomeMovieCard movieImage={MoviePoster}/>
-            <HomeMovieCard movieImage={MoviePoster}/>
-            <HomeMovieCard movieImage={MoviePoster}/>
-
-        </div>
-        Home
+                </div>
+                <HomeBanners image={IccBannerImage} />
+                <HomeFooter />
+            </div>
         </>
     )
+
 }
 
 export default Home;
